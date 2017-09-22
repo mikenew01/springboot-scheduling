168fad4cc9d67e7aa3fc0aa9f7534eff8d707235
Maikon Canuto <maikon.pedrosa@altran-ais.com.br>
2017-09-22 11:33:09 -0300

    Alteração de nomes dos metodos

--- src/main/java/br/com/mk/springscheduling/jobs/ExemploJob.java
+++ src/main/java/br/com/mk/springscheduling/jobs/ExemploJob.java
@@ -17,58 +17,58 @@ public class ExemploJob {
     private static final Logger log = LoggerFactory.getLogger(ExemploJob.class);
 
     /**
-        @Scheduled(fixedRate = 5000): Especifica um intervalo entre as invocações a partir do inicio de cada invocação.
+     * @Scheduled(fixedRate = 5000): Especifica um intervalo entre as invocações a partir do inicio de cada invocação.
      */
     @Scheduled(fixedRate = 5000)
-    public void task1() {
+    public void scheduledFixedRate() {
         log.info("Executando task 1 - " + LocalDateTime.now());
     }
 
     /**
-        @Scheduled(fixedDelay = 5000): Especifica um intervalo entre as invocações a partir da conclusão da tarefa
-        anterior.
+     * @Scheduled(fixedDelay = 5000): Especifica um intervalo entre as invocações a partir da conclusão da tarefa
+     * anterior.
      */
     @Scheduled(fixedDelay = 5000)
-    public void task2() {
+    public void scheduledFixedDelay() {
         log.info("Executando task 2 - " + LocalDateTime.now());
     }
 
     /**
-        @Scheduled(initialDelay = 1000, fixedRate = 5000): Especifica um delay inicial antes e executar a tarefa.
+     * @Scheduled(initialDelay = 1000, fixedRate = 5000): Especifica um delay inicial antes e executar a tarefa.
      */
     @Scheduled(initialDelay = 1000, fixedRate = 5000)
-    public void task3() {
+    public void scheduledInitialDelay() {
         log.info("Executando task 3 - " + LocalDateTime.now());
     }
 
     /**
-        [minutos] [horas] [dias do mês] [mês] [dias da semana] [usuário] [comando]
-
-        O preenchimento de cada campo é feito da seguinte maneira:
-        Minutos: informe números de 0 a 59;
-        Horas: informe números de 0 a 23;
-        Dias do mês: informe números de 1 a 31;
-        Mês: informe números de 1 a 12;
-        Dias da semana: informe números de 0 a 7;
+     * [minutos] [horas] [dias do mês] [mês] [dias da semana] [usuário] [comando]
+     * <p>
+     * O preenchimento de cada campo é feito da seguinte maneira:
+     * Minutos: informe números de 0 a 59;
+     * Horas: informe números de 0 a 23;
+     * Dias do mês: informe números de 1 a 31;
+     * Mês: informe números de 1 a 12;
+     * Dias da semana: informe números de 0 a 7;
      */
     @Scheduled(cron = "*/10 * * * * MON-FRI")
-    public void task4() {
+    public void scheduledCron() {
         log.info("Executando task 4 - " + LocalDateTime.now());
     }
 
     /**
-        @Scheduled(fixedRateString = "${exemplo.job.fixedRate}"): Parametrização dos valores para scheduled fixedRate
+     * @Scheduled(fixedRateString = "${exemplo.job.fixedRate}"): Parametrização dos valores para scheduled fixedRate
      */
     @Scheduled(fixedRateString = "${exemplo.job.fixedRate}")
-    public void task5() {
+    public void scheduledFixedRateString() {
         log.info("Executando task 5 - " + LocalDateTime.now());
     }
 
     /**
-        @Scheduled(fixedDelayString = "${exemplo.job.fixedDelay}"): Parametrização dos valores para scheduled fixedDelay
+     * @Scheduled(fixedDelayString = "${exemplo.job.fixedDelay}"): Parametrização dos valores para scheduled fixedDelay
      */
     @Scheduled(fixedDelayString = "${exemplo.job.fixedDelay}")
-    public void task6() {
+    public void scheduledFixedDelayString() {
         log.info("Executando task 6 - " + LocalDateTime.now());
     }
 }
